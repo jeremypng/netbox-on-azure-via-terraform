0b6f8be7479ad81ad1cc5edf37d034d9daabd371
jeremypng <jsanders@cspire.com>
2020-02-18 23:48:03 -0600

    install script

--- netbox-install.sh
+++ netbox-install.sh
@@ -1,4 +1,4 @@
-sudo apt-get update -q=2
-sudo apt-get upgrade -q=2
-sudo apt-get autoremove -q=2
-sudo apt-get install postgresql
+apt-get update -q=2
+apt-get upgrade -q=2
+apt-get autoremove -q=2
+apt-get install postgresql libpq-dev
