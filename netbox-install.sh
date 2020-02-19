314124b5fc1f6a3a6e3b027ff4221836a07c8300
jeremypng <jsanders@cspire.com>
2020-02-19 11:43:28 -0600

    install script

--- netbox-install.sh
+++ netbox-install.sh
@@ -5,5 +5,5 @@ apt-get -q=2 install postgresql libpq-dev
 systemctl start postgresql
 systemctl enable postgresql
 export PGSQL_PASS=`date +%s | sha256sum | base64 | head -c 32 ; echo`
-echo "export PGSQL_PASS=$PGSQL_PASS" >> /home/postgres/.bashrc
+echo "export PGSQL_PASS=$PGSQL_PASS" >> /var/lib/postgresql/.bashrc
 curl https://raw.githubusercontent.com/jeremypng/netbox-on-azure-via-terraform/master/postgres-config.sh | su -l postgres bash
