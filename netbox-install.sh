a4ab9ff8fdc488f18464ba28a0db79e80fd40c88
jeremypng <jsanders@cspire.com>
2020-02-19 00:35:59 -0600

    install script

--- netbox-install.sh
+++ netbox-install.sh
@@ -1,4 +1,3 @@
-su -
 apt-get update -q=2
 apt-get upgrade -q=2
 apt-get autoremove -q=2
@@ -7,4 +6,4 @@ systemctl start postgresql-9.6
 systemctl enable postgresql-9.6
 PGSQL_PASS=`date +%s | sha256sum | base64 | head -c 32 ; echo`
 echo "export PGSQL_PASS=$PGSQL_PASS" >> .bashrc
-su - postgres echo '\x \\ CREATE DATABASE netbox; \\ CREATE USER netbox WITH PASSWORD '$PGSQL_PASS'; \\ GRANT ALL PRIVILEGES ON DATABASE netbox TO netbox; \\ /q' | psql
+echo '\x \\ CREATE DATABASE netbox; \\ CREATE USER netbox WITH PASSWORD '$PGSQL_PASS'; \\ GRANT ALL PRIVILEGES ON DATABASE netbox TO netbox; \\ /q' | su - postgres psql
